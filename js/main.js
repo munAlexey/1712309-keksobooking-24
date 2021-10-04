0509f7569814b6319f48670db43bb88122e02569
alexmun <mun.leshka@gmail.com>
2021-10-04 13:42:33 +0900

    поправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 function getRandomNumber (from, to) {
   if (to < 0) {
-    return console.log('Введите положительное число > 0 || == 0');
+    return 'Введите положительное число > 0 || == 0';
   }
-    from = Math.ceil(from);
-    to = Math.floor(to);
-    return Math.floor(Math.random() * (to - from)) + from;
+  from = Math.ceil(from);
+  to = Math.floor(to);
+  return Math.floor(Math.random() * (to - from)) + from;
 }
 
-console.log(getRandomNumber(2,10));
+getRandomNumber(2,10);
 
 function getRandomCoor (from, to, reminder) {
   if (from >= to) {
-    return ((Math.random() * (from - to)) + to).toFixed(reminder);;
+    return Number(((Math.random() * (from - to)) + to).toFixed(reminder));
   }
-  return ((Math.random() * (to - from)) + from).toFixed(reminder);
+  return Number(((Math.random() * (to - from)) + from).toFixed(reminder));
 }
 
-console.log(getRandomCoor(20, 30, 5));
+getRandomCoor(20, 30, 5);
